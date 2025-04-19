519756e74a12e0d3f75eb0fbaf273e8c1a65640c
Nadman Faisal <nadman@chalmers.se>
2025-04-19 19:49:15 +0200

    Initialize adns

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Need the following for ADNS lib installation:-
 sudo apt-get update
 sudo apt-get install libadns1-dev
 ```
+
+To compile:-
+
+gcc main.c -o main -ladns %% ./main
--- main.c
+++ main.c
@@ -2,3 +2,28 @@
 #include <stdlib.h>
 #include <adns.h>
 
+#define MAX_CHARS 256
+
+void clear_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
+
+int main(int argc, char **argv) {
+    adns_state state;
+    adns_query query;
+    adns_initflags flag = adns_if_none;
+    FILE *log_file = fopen("log.txt", "w");
+
+    int result = adns_init(&state, flag, log_file);
+
+    printf("Result: %d\n", result);
+
+    char user_input[MAX_CHARS];
+    printf("Provide a domain address: ");
+    scanf("%s", user_input);
+    clear_buffer();
+
+    printf("User input: %s\n", user_input);
+    adns_finish(state);
+}
