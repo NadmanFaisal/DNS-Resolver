f4bd8d4fc7e572186e4a7bd701f4c8264d1aba71
Nadman Faisal <nadman@chalmers.se>
2025-04-19 20:07:51 +0200

    Submit query and wait for result

--- main.c
+++ main.c
@@ -12,18 +12,15 @@ void clear_buffer() {
 int main(int argc, char **argv) {
     adns_state state;
     adns_query query;
-    adns_initflags flag = adns_if_none;
     FILE *log_file = fopen("log.txt", "w");
+    int init = adns_init(state, adns_if_none, log_file);
+    
+    const char owner[MAX_CHARS] = "www.google.com";
+    void *context = NULL;
+    int result = adns_submit(state, owner, adns_r_a, adns_if_none, context, &query);
 
-    int result = adns_init(&state, flag, log_file);
+    adns_answer *answer;
+    int wait_res = adns_wait(state, &query, &answer, &context);
 
-    printf("Result: %d\n", result);
-
-    char user_input[MAX_CHARS];
-    printf("Provide a domain address: ");
-    scanf("%s", user_input);
-    clear_buffer();
-
-    printf("User input: %s\n", user_input);
-    adns_finish(state);
+    // printf("Result: %d\n", init);
 }
